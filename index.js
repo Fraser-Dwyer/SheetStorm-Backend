fb54ac5822b843a8154e08b7cf8caf066a88a634
Fraser Dwyer <fraserd1413@gmail.com>
2023-11-14 15:35:48 +0000

    Users registeration works

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const User = require("./models/User");
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
-app.post("/signup", (req, res) => {
-  res.json("test ok");
+mongoose.connect(
+  "mongodb+srv://testUser:testUser123@cluster0.ksotpfo.mongodb.net/?retryWrites=true&w=majority"
+);
+
+app.post("/signup", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const userDoc = await User.create({ username, password });
+    res.json(userDoc);
+  } catch (e) {
+    console.log(e);
+    res.status(400).json(e);
+  }
 });
 
 app.listen(4000);
