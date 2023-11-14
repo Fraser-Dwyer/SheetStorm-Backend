527341230b95d58c3db32e34fe9bf0d056022158
Fraser Dwyer <fraserd1413@gmail.com>
2023-11-14 16:54:04 +0000

    jwt added

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const User = require("./models/User");
 const bcrypt = require("bcryptjs");
-
 const app = express();
+const jwt = require("jsonwebtoken");
 
 const salt = bcrypt.genSaltSync(10);
+const secret = "sdkjfbn239rdskb2398ds";
 
-app.use(cors());
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
 
 mongoose.connect(
@@ -33,7 +34,15 @@ app.post("/login", async (req, res) => {
   const { username, password } = req.body;
   const userDoc = await User.findOne({ username });
   const passwordOk = bcrypt.compareSync(password, userDoc.password);
-  res.json(passwordOk);
+  if (passwordOk) {
+    // User is logged in
+    jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+      if (err) throw err;
+      res.cookie("token", token).json("ok");
+    });
+  } else {
+    res.status(400).json("Invalid login");
+  }
 });
 
 app.listen(4000);
