7b767bb9192878e65c223bcde08ffc32dc2d8d83
Fraser Dwyer <fraserd1413@gmail.com>
2023-11-14 16:15:58 +0000

    Login endpoint now works

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const User = require("./models/User");
+const bcrypt = require("bcryptjs");
+
 const app = express();
+
+const salt = bcrypt.genSaltSync(10);
 
 app.use(cors());
 app.use(express.json());
@@ -14,12 +18,22 @@ mongoose.connect(
 app.post("/signup", async (req, res) => {
   const { username, password } = req.body;
   try {
-    const userDoc = await User.create({ username, password });
+    const userDoc = await User.create({
+      username,
+      password: bcrypt.hashSync(password, salt),
+    });
     res.json(userDoc);
   } catch (e) {
     console.log(e);
     res.status(400).json(e);
   }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const userDoc = await User.findOne({ username });
+  const passwordOk = bcrypt.compareSync(password, userDoc.password);
+  res.json(passwordOk);
 });
 
 app.listen(4000);
